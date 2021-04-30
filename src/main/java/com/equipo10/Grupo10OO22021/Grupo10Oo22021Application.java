1a59ad8b9e1d860e3907f156864b30c7a15f68fa
MarianoDigangi1 <marianodigangi1@gmail.com>
2021-04-29 23:23:45 -0300

    base del proyecto

---
+++ src/main/java/com/equipo10/Grupo10OO22021/Grupo10Oo22021Application.java
@@ -0,0 +1,13 @@
+package com.equipo10.Grupo10OO22021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo10Oo22021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo10Oo22021Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/equipo10/Grupo10OO22021/Grupo10Oo22021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipo10.Grupo10OO22021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo10Oo22021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
